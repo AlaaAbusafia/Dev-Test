6933e21f8a73c2bb0434bde9f8b65b164c1e8494
Alaa <alaa@Ahmeds-Air.lan>
2021-03-02 13:50:45 +1300

    Add comments for each function and console.log

--- test.js
+++ test.js
@@ -1,72 +1,62 @@
-
-reformat = (string) =>{
-    
-    var noVowel = string.replace(/[aeiou]/gi, '');
-    var str = noVowel.charAt(0).toUpperCase() + noVowel.slice(1);
-    var result = str.charAt(0) + str.slice(1).toLowerCase();
-    return result;
-    
-}
-
-
+//### Task 1:
+// Function takes an array and repeat it triple times
  const repeat = (arr) =>
   [].concat(...Array.from({ length: 3 }, () => arr));
-  
-console.log(repeat([1, 2, 3]));
 
+console.log(repeat([1, 2, 3])); //[1,2,3,1,2,3,1,2,3]  
+
+
+//### Task 2:
+//Function that take a string and delete the vowels, 
+// and make all leters lowercase except the first letter
+
+reformat = (string) => {
+    // delete the vowels
+    var noVowel = string.replace(/[aeiou]/gi, '');
+    
+    // Make the first letter upperCase
+    var str = noVowel.charAt(0).toUpperCase() + noVowel.slice(1);
+    
+    // Make the letters lowercase starting from the second one
+    var result = str.charAt(0) + str.slice(1).toLowerCase();
+    
+    return result; 
+}
+console.log(reformat("liMeSHArp DeveLoper TEST") ); //"Lmshrp dvlpr tst"
+
+
+
+
+//### Task 3
+//Function that take a binary number as an arrey and return the next
+//binary naumber without using any built in functions
 
 next_binary_number = arr =>{
   
     const arrLength = arr.length; 
     var i;
-  
+    
+    // Find first 0 from right side and loop
     for(i= arrLength-1; i >= 0; i--){
         
+        // convert first 0 to 1 and break
         if (arr[i] == 0){ 
             arr[i] = 1; 
             break; 
-
         }
+         // else convert 1 to 0 
         else{ 
             arr[i] = 0; 
         }    
     }
 
+    // if the binary array contains only the set bits
     if (i < 0) { 
         arr = [1, ...arr]; 
-    } 
+    }
+    //result of the next binary
     return arr; 
 }
 
+console.log(next_binary_number([1,0])); // [1,1]
 
-
-
-next_binary_number = arr =>{
-  
-    const arrLength = arr.length; 
-    var i;
-
-    for(i= arrLength-1; i >= 0; i--){
-        
-        if (arr[i] == 0){ 
-            arr[i] = 1; 
-            
-            break; 
-  
-       
-        }else{ 
-            arr[i] = 0; 
-  
-        }
-    arr.map(function increment(arr) {
-      return arr + 1;
-    });
-    }
-    
-if (i < 0) { 
-        arr = [1, ...arr]; 
-    }
-
-     
-    return arr; 
-}
